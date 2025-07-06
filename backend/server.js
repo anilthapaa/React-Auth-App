24e1f6d61dfac2467f5e86d6933a1d3852588c91
Anil Thapa <thapaanil0@gmai.com>
2025-07-06 17:48:48 -0400

    Switched Database to cloud from local environment

--- backend/server.js
+++ backend/server.js
@@ -63,6 +63,8 @@ app.get('/api/user', async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log('✅ Backend running on http://localhost:5000');
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`✅ Backend running on http://localhost:${PORT}`);
 });
