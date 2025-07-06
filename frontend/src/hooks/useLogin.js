0d2d52e91ca7cc05ddcb1ed76dcf5cf9ba3a996d
Anil Thapa <thapaanil0@gmai.com>
2025-07-06 19:27:54 -0400

    API to Backend Added.

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -7,7 +7,7 @@ export const useLogin = () => {
   const login = async (username, password) => {
     setLoading(true);
     try {
-      const res = await fetch('http://localhost:5000/api/login', {
+      const res = await fetch(`${process.env.REACT_APP_API_URL}/api/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password })
