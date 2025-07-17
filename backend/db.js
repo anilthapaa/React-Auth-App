de0d43749221305cca9761c878636fe79808eed1
Anil Thapa <thapaanil0@gmai.com>
2025-07-17 11:52:32 -0400

    Workflow Error Fixing...

--- backend/db.js
+++ backend/db.js
@@ -1,3 +1,4 @@
+/* eslint-env node */
 const mysql = require('mysql2');
 const fs = require('fs');
 require('dotenv').config();
--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,4 +1,5 @@
-import { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState } from "react";
+import PropTypes from "prop-types";
 
 const AuthContext = createContext();
 
@@ -9,6 +10,10 @@ export const AuthProvider = ({ children }) => {
 
 export const useAuth = () => useContext(AuthContext);
 
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 // This code creates an AuthContext using React's Context API to manage user authentication state.
 // The AuthProvider component wraps the application and provides the user state and setUser function to its children.
